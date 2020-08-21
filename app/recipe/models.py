fe937a6e98df80c2311d4136f7be895c2784f9f5
Damian Nardelli <damian.nardelli@travelperk.com>
2020-08-21 12:46:54 -0300

    (fix for PR review) - fix docstring and adding Meta in each model to improve Django Admin site

--- app/recipe/models.py
+++ app/recipe/models.py
@@ -2,22 +2,30 @@ from django.db import models
 
 
 class Recipe(models.Model):
-    """Recipe object"""
+    """Recipe model"""
     name = models.TextField()
     description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
 
     def __str__(self):
         return self.name
 
 
 class Ingredient(models.Model):
-    """Ingredient object"""
+    """Ingredient model"""
     name = models.TextField()
     recipe = models.ForeignKey(
         'Recipe',
         related_name='ingredients',
         on_delete=models.CASCADE
     )
+
+    class Meta:
+        verbose_name = 'Ingredient'
+        verbose_name_plural = 'Ingredients'
 
     def __str__(self):
         return self.name
