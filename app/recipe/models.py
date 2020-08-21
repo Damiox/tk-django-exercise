5b91ac67c96524bcb558535f0ec2e5fd7fabbb90
Damian Nardelli <damian.nardelli@travelperk.com>
2020-08-21 12:40:06 -0300

    (fix for PR review) - getting rid of CharField and using TextField so we don't have to specify a max_length setting

--- app/recipe/models.py
+++ app/recipe/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Recipe(models.Model):
     """Recipe object"""
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=4096)
+    name = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class Recipe(models.Model):
 
 class Ingredient(models.Model):
     """Ingredient object"""
-    name = models.CharField(max_length=255)
+    name = models.TextField()
     recipe = models.ForeignKey(
         'Recipe',
         related_name='ingredients',
